0df9f384b2bb5a8252e7febf255c3922202a2a2c
DavidRivasM <alonsorivasmora@gmail.com>
2021-02-06 05:44:47 +0000

    version 1

--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,32 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+// let contador = 1;
+
+// ReactDOM.render(
+// 	<Home digito1={1} digito2={1} digito3={1} digito4={1} />,
+// 	document.querySelector("#app")
+// );
+
+let contador = 0;
+let minuto = 0;
+let hora = 0;
+setInterval(function() {
+	let one = Math.floor(contador / 1);
+	//render your react application
+	contador++;
+
+	if (contador > 59) {
+		contador = 0;
+		minuto = minuto + 1;
+	} else if (minuto > 59) {
+		hora = hora + 1;
+		minuto = 0;
+	}
+
+	ReactDOM.render(
+		<Home segundo={one} minuto={minuto} hora={hora} />,
+		document.querySelector("#app")
+	);
+}, 1000);
